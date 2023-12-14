091eb3d1db5dd6da38e00730762e77cc9816fceb
Fredrik Olsson <freol@outlook.com>
2023-12-14 14:15:42 +0100

    Update for current installation at Gota

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 
 docker-compose \
     -f docker-compose.zenoh-router.yml \
+    -f docker-compose.ais.yml \
     -f docker-compose.cameras.yml \
+    -f docker-compose.logrotate.yml \
     -f docker-compose.nmea.yml \
+    -f docker-compose.radar.yml \
     up -d
